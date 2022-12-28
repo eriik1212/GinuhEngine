6732efc752ea0dee8b7a98679a3cc49de769998b
VoZeS <davidboces12@hotmail.com>
2022-12-28 19:06:05 +0100

    WwiseAlmostDone

--- Engine/ModuleAudio.cpp
+++ Engine/ModuleAudio.cpp
@@ -4,7 +4,7 @@
 
 ModuleAudio::ModuleAudio(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
-	
+
 }
 
 // Destructor
@@ -17,12 +17,12 @@ bool ModuleAudio::Init()
 	//LOG("Initializing Audio context");
 	App->menus->info.AddConsoleLog("Initializing Audio context");
 
-    //InitMemoryManager();
-    //InitStreamingManager();
-    //InitSoundEngine();
-    //InitMusicEngine();
-    ////InitSpatialAudio();
-    //InitCommunications();
+	InitMemoryManager();
+	InitStreamingManager();
+	InitSoundEngine();
+	InitMusicEngine();
+	//InitSpatialAudio();
+	//InitCommunications();
 
 	bool ret = true;
 
@@ -39,7 +39,7 @@ update_status ModuleAudio::PreUpdate(float dt)
 // PostUpdate present buffer to screen
 update_status ModuleAudio::PostUpdate(float dt)
 {
-    //ProcessAudio();
+	//ProcessAudio();
 
 	return UPDATE_CONTINUE;
 }
@@ -50,12 +50,140 @@ bool ModuleAudio::CleanUp()
 	//LOG("Destroying 3D Renderer");
 	App->menus->info.AddConsoleLog("Destroying Module Audio");
 
-    //TermCommunicationModule();
-    ////TermSpatialAudio();  // NO TERM FUNCTION :(
-    //TermMusicEngine();
-    //TermSoundEngine();
-    //TermStreamingManager();
-    //TermMemoryManager();
+	//TermCommunicationModule();
+	////TermSpatialAudio();  // NO TERM FUNCTION :(
+	//TermMusicEngine();
+	//TermSoundEngine();
+	//TermStreamingManager();
+	//TermMemoryManager();
 
 	return true;
 }
+
+bool ModuleAudio::InitMemoryManager()
+{
+	AkMemSettings memSettings;
+	AK::MemoryMgr::GetDefaultSettings(memSettings);
+
+	if (AK::MemoryMgr::Init(&memSettings) != AK_Success)
+	{
+		assert(!"Could not create the memory manager.");
+		return false;
+	}
+
+	return true;
+}
+
+bool ModuleAudio::InitStreamingManager()
+{
+	//
+   // Create and initialize an instance of the default streaming manager. Note
+   // that you can override the default streaming manager with your own. 
+   //
+
+	AkStreamMgrSettings stmSettings;
+	AK::StreamMgr::GetDefaultSettings(stmSettings);
+
+	// Customize the Stream Manager settings here.
+
+	if (!AK::StreamMgr::Create(stmSettings))
+	{
+		assert(!"Could not create the Streaming Manager");
+		return false;
+	}
+
+	//
+	// Create a streaming device with blocking low-level I/O handshaking.
+	// Note that you can override the default low-level I/O module with your own. 
+	//
+	AkDeviceSettings deviceSettings;
+	AK::StreamMgr::GetDefaultDeviceSettings(deviceSettings);
+
+	// Customize the streaming device settings here.
+
+	// CAkFilePackageLowLevelIOBlocking::Init() creates a streaming device
+	// in the Stream Manager, and registers itself as the File Location Resolver.
+	/*if (g_lowLevelIO.Init(deviceSettings) != AK_Success)
+	{
+		assert(!"Could not create the streaming device and Low-Level I/O system");
+		return false;
+	}*/
+
+	return true;
+}
+
+bool ModuleAudio::InitSoundEngine()
+{
+	//
+   // Create the Sound Engine
+   // Using default initialization parameters
+   //
+
+	AkInitSettings initSettings;
+	AkPlatformInitSettings platformInitSettings;
+	AK::SoundEngine::GetDefaultInitSettings(initSettings);
+	AK::SoundEngine::GetDefaultPlatformInitSettings(platformInitSettings);
+
+	if (AK::SoundEngine::Init(&initSettings, &platformInitSettings) != AK_Success)
+	{
+		assert(!"Could not initialize the Sound Engine.");
+		return false;
+	}
+
+	return true;
+}
+
+bool ModuleAudio::InitMusicEngine()
+{
+
+	//
+	// Initialize the music engine
+	// Using default initialization parameters
+	//
+
+	AkMusicSettings musicInit;
+	AK::MusicEngine::GetDefaultInitSettings(musicInit);
+
+	if (AK::MusicEngine::Init(&musicInit) != AK_Success)
+	{
+		assert(!"Could not initialize the Music Engine.");
+		return false;
+	}
+
+	return true;
+}
+
+//bool ModuleAudio::InitSpatialAudio()
+//{
+//	//
+//	// Initialize Spatial Audio
+//	// Using default initialization parameters
+//	//
+//
+//	AkSpatialAudioInitSettings settings; // The constructor fills AkSpatialAudioInitSettings with the recommended default settings. 
+//	if (AK::SpatialAudio::Init(settings) != AK_Success)
+//	{
+//		assert(!"Could not initialize the Spatial Audio.");
+//		return false;
+//	}
+//
+//	return true;
+//}
+
+//bool ModuleAudio::InitCommunications()
+//{
+//#ifndef AK_OPTIMIZED
+//	//
+//	// Initialize communications (not in release build!)
+//	//
+//	AkCommSettings commSettings;
+//	AK::Comm::GetDefaultInitSettings(commSettings);
+//	if (AK::Comm::Init(commSettings) != AK_Success)
+//	{
+//		assert(!"Could not initialize communication.");
+//		return false;
+//	}
+//#endif // AK_OPTIMIZED
+//
+//	return true;
+//}
--- Engine/ModuleAudio.h
+++ Engine/ModuleAudio.h
@@ -4,6 +4,53 @@
 #include "Module.h"
 #include "Globals.h"
 
+#include <AK/SoundEngine/Common/AkSoundEngine.h>	   // SoundEngine
+#include <AK/MusicEngine/Common/AkMusicEngine.h>	   // MusicEngine
+#include <AK/SoundEngine/Common/AkMemoryMgr.h>         // Memory Manager
+#include <AK/SoundEngine/Common/AkModule.h>            // Default memory and stream managers  
+#include <AK/SoundEngine/Common/AkStreamMgrModule.h>   // Streaming Manager
+#include <AK/SoundEngine/Common/AkTypes.h>
+#include <AK/Tools/Common/AkPlatformFuncs.h>           // Thread defines
+#include <AK/SpatialAudio/Common/AkSpatialAudio.h>              // Spatial Audio
+
+#ifdef _DEBUG
+	// Input libraries
+	#pragma comment( lib, "Wwise/Debug(StaticCRT)/lib/AkSoundEngine.lib")
+	#pragma comment( lib, "Wwise/Debug(StaticCRT)/lib/AkMusicEngine.lib")
+	#pragma comment( lib, "Wwise/Debug(StaticCRT)/lib/AkMemoryMgr.lib")
+	#pragma comment( lib, "Wwise/Debug(StaticCRT)/lib/AkStreamMgr.lib")
+
+	// External library dependencies
+	#pragma comment( lib, "Wwise/dinput8.lib") // Microsoft DirectX DirectInput.Needed by Motion to support DirectInput devices.
+	#pragma comment( lib, "Wwise/dsound.lib")  // Microsoft DirectX DirectSound library
+	#pragma comment( lib, "Wwise/dxguid.lib")  // Microsoft DirectX Audio GUIDs
+	#pragma comment( lib, "Wwise/WinMM.Lib")  // Microsoft Multimedia API. Needed by Motion for internal device targeting management. 
+	#pragma comment( lib, "Wwise/WS2_32.Lib")  // Microsoft Winsock 2 library (used for Wwise profiling) 
+	#pragma comment( lib, "Wwise/XInput.lib")  // Microsoft XInput. Needed by Motion to support XInput devices (Xbox controller).
+#else
+#define AK_OPTIMIZED
+	// Input libraries
+	//#pragma comment( lib, "Wwise/Release/lib/AkMusicEngine.lib")
+	//#pragma comment( lib, "Wwise/Release/lib/AkMemoryMgr.lib")
+	//#pragma comment( lib, "Wwise/Release/lib/AkStreamMgr.lib")
+	//#pragma comment( lib, "Wwise/Release/lib/AkSoundEngine.lib")
+
+	// External library dependencies
+	#pragma comment( lib, "Wwise/dinput8.lib") // Microsoft DirectX DirectInput.Needed by Motion to support DirectInput devices.
+	#pragma comment( lib, "Wwise/dsound.lib")  // Microsoft DirectX DirectSound library
+	#pragma comment( lib, "Wwise/dxguid.lib")  // Microsoft DirectX Audio GUIDs
+	#pragma comment( lib, "Wwise/WinMM.Lib")  // Microsoft Multimedia API. Needed by Motion for internal device targeting management. 
+	//#pragma comment( lib, "Wwise/WS2_32.Lib")  // Microsoft Winsock 2 library (used for Wwise profiling) 
+	#pragma comment( lib, "Wwise/XInput.lib")  // Microsoft XInput. Needed by Motion to support XInput devices (Xbox controller).
+#endif
+
+//#include <AkFilePackageLowLevelIOBlocking.h>                    // Sample low-level I/O implementation
+
+// Include for communication between Wwise and the game -- Not needed in the release version
+#ifndef AK_OPTIMIZED
+	#include <AK/Comm/AkCommunication.h>
+#endif // AK_OPTIMIZED
+
 class ModuleAudio : public Module
 {
 public:
@@ -18,10 +65,10 @@ public:
 private:
 
 	//// ----------------------------------- Init Functions
-	//bool InitMemoryManager();
-	//bool InitStreamingManager();
-	//bool InitSoundEngine();
-	//bool InitMusicEngine();
+	bool InitMemoryManager();
+	bool InitStreamingManager();
+	bool InitSoundEngine();
+	bool InitMusicEngine();
 	//bool InitSpatialAudio(); // INIT OK!
 	//bool InitCommunications();
 
